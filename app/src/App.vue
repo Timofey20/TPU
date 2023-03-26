3e1ce210ce488341e44a2740d4b4d9d60787bd48
Timofey <timofey20ab@gmail.com>
2023-03-26 18:51:30 +0700

    Add validate

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,7 +2,6 @@
   <div class="container">
     <h2>Исходные данные</h2>
     <FormDefault />
-    <button @click="formCheck()" type="button" class="btn btn-primary mb-3">Посчитать</button>
   </div>
 </template>
 
@@ -12,15 +11,6 @@ import '@/styles/style.scss'
 
 export default {
   name: 'App',
-  methods: {
-    formCheck() {
-      setTimeout(() => {
-        alert('Объект защищен')
-      }, 1000);
-    }
-  },
-  props: {
-  },
   components: { FormDefault }
 }
 </script>
